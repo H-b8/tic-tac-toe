194bdc5c8db11ea55a8aa4caa3b644acd67023c5
CL <carolinemv.le@gmail.com>
2020-02-05 18:35:42 -0800

    wins turn red

--- js/main.js
+++ js/main.js
@@ -6,9 +6,14 @@ const lookup = {
     'null': ' '
 };
 
+const winningCombos = [ [0, 1, 2], [3, 4, 5], [6, 7, 8],
+[0, 3, 6], [1, 4, 7], [2, 5, 8],
+[0, 4, 8], [2, 4, 6]
+];
+
 /*----- app's state (variables) -----*/
 
-let boardArr, turn, winner;
+let turn, winner;
 
 /*----- cached element references -----*/
 
@@ -16,18 +21,31 @@ let boxes = document.querySelector('div');
 
 /*----- event listeners -----*/
 
-// addEventListener is a higher order function
-// these are callback functions b/c they call functions within a function
-
-document.querySelector('section').addEventListener('click', makeMove);
 document.querySelector('button').addEventListener('click', initialize);
+// figure out how to hide while game is being played
+
+let section = document.querySelector('section')
+section.addEventListener('click', (evt) => {
+    if(board[evt.target.id] !== null) {
+        console.log("THIS BOX IS TAKEN")
+        // render()
+    } 
+    else {
+        makeMove(evt);
+    }
+});
 
 /*----- functions -----*/
 
 initialize();
 
 function initialize() {
-    console.log('reset')
+    console.log('GAME STARTED')
+    replay.innerText = "'X' GOES FIRST :^)";
+    let boxes = section.children;
+    for(let i = 0; i < boxes.length; i++ ){
+        boxes[i].innerText = '';
+    }
     board = [null, null, null, 
         null, null, null, 
         null, null, null];
@@ -36,47 +54,38 @@ function initialize() {
     // render();
 }
 
-function render() {
-   
+function render() { // display messages
+    
 }
 
 function makeMove(evt) {
-    console.log(turn)
     let i = evt.target.id;
-    console.log('clicked ' + i)
+    console.log('player ' + turn + ' clicked box ' + i);
+
+    if (turn === 1) {
+        evt.target.innerText = 'X';
+        replay.innerText = "O's TURN!";
+    }
+    else {
+        evt.target.innerText = 'O';
+        replay.innerText = "X's TURN!";
+    }
+
     board[i] = turn;
-    turn *= -1; // switches turn
     winner = checkWinner();
-    // render();
+    turn *= -1;
 }
 
 function checkWinner() {
     console.log(board)
-    if (Math.abs(board[0]+board[1]+board[2]) === 3) {
-        console.log('winner')
-    }
-    else if (Math.abs(board[3]+board[4]+board[5]) === 3) {
-        console.log('winner')
-    }
-    else if (Math.abs(board[6]+board[7]+board[8]) === 3) {
-        console.log('winner')
-    }
-    else if (Math.abs(board[0]+board[3]+board[6]) === 3) {
-        console.log('winner')
-    }
-    else if (Math.abs(board[1]+board[4]+board[7]) === 3) {
-        console.log('winner')
-    }
-    else if (Math.abs(board[2]+board[5]+board[8]) === 3) {
-        console.log('winner')
-    }
-    else if (Math.abs(board[0]+board[4]+board[8]) === 3) {
-        console.log('winner')
-    }
-    else if (Math.abs(board[2]+board[4]+board[6]) === 3) {
-        console.log('winner')
-    }
-    else {
-        console.log('no winner yet')
-    } 
+
+    winningCombos.forEach(function(combo) {
+
+        if (Math.abs(board[combo[0]]+board[combo[1]]+board[combo[2]]) === 3) {
+            document.getElementById(combo[0]).style.color = 'red';
+            document.getElementById(combo[1]).style.color = 'red';
+            document.getElementById(combo[2]).style.color = 'red';
+            replay.innerText = "RESET GAME";
+        }
+    });
 }
